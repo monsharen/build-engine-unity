80a3ac9a319f6429233d4d4695fc4d62a0d98762
thomas-rosenquist <thomas.rosenquist@unity3d.com>
2024-01-17 21:21:55 +0100

    namespace

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,33 +1,28 @@
-
+using BuildEngine;
 using BuildEngineMapReader;
-
 using DefaultNamespace;
 using UnityEngine;
 
-
-
-namespace BuildEngine
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Game : MonoBehaviour
 {
-    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-    public class Game : MonoBehaviour
-    {
         
-        public GameObject rootNode;
+    public GameObject rootNode;
 
-        private TextureManager _textureManager;
+    private TextureManager _textureManager;
         
-        private void Start()
-        {
-            _textureManager = new TextureManager();
+    private void Start()
+    {
+        _textureManager = new TextureManager();
         
-            var mapFileReader = new MapFileReader();
-            var fileName = AssetManager.GetMapAssetPath("THE_BASE.MAP");
-            var map = mapFileReader.ReadFile(fileName);
-            Debug.Log(map);
+        var mapFileReader = new MapFileReader();
+        var fileName = AssetManager.GetMapAssetPath("THE_BASE.MAP");
+        var map = mapFileReader.ReadFile(fileName);
+        Debug.Log(map);
             
-            var mapRenderer = new MapRenderer(rootNode, _textureManager);
-            mapRenderer.Render(map);
-        }
+        var mapRenderer = new MapRenderer(rootNode, _textureManager);
+        mapRenderer.Render(map);
+    }
 
         
 
@@ -35,5 +30,4 @@ namespace BuildEngine
     
         
 
-    }
 }
